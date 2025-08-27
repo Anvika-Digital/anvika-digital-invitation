235c9ae2d7f6f01f62d67bad45492f840d8bb16e
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2025-08-27 21:57:59 +0800

    style: add animation and transition to welcome message

--- src/app/[locale]/(public)/invitation/tata-dan-widya/_components/welcome-modal.tsx
+++ src/app/[locale]/(public)/invitation/tata-dan-widya/_components/welcome-modal.tsx
@@ -18,9 +18,9 @@ export default function WelcomeModal({ guestName }: WelcomeModalProps) {
       {isOpen && (
         <motion.section
           className="absolute inset-0 z-50 h-full w-full overflow-hidden bg-white"
-          initial={{ x: 0 }}
-          animate={{ x: 0 }}
-          exit={{ x: '-100%' }}
+          initial={{ x: 0, opacity: 1 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: '-100%', opacity: 0 }}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
           key="welcome-message"
         >
@@ -31,16 +31,50 @@ export default function WelcomeModal({ guestName }: WelcomeModalProps) {
             {/* Welcome Message */}
             <BaseContainer className="relative z-20 flex h-full flex-col justify-between text-white">
               <div className="font-playfair space-y-2 text-center">
-                <h2 className="text-xl">The Wedding Of</h2>
-                <h1 className="text-5xl font-medium">Gus Tata & Dayu Widya</h1>
+                <motion.h2
+                  initial={{ y: -20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.5, ease: 'easeOut', delay: 1 }}
+                  className="text-xl"
+                >
+                  The Wedding Of
+                </motion.h2>
+                <motion.h1
+                  initial={{ y: -20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.5, ease: 'easeOut', delay: 1.5 }}
+                  className="text-5xl font-medium"
+                >
+                  Gus Tata & <br /> Dayu Widya
+                </motion.h1>
               </div>
 
               <div className="mt-auto space-y-4 text-center text-white">
-                <p className="font-source-sans">Kepada Yth.</p>
-                <p className="mb-6 text-3xl">{guestName}</p>
-                <Button onClick={() => setIsOpen(false)} size="lg">
-                  Buka Undangan
-                </Button>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, ease: 'easeOut', delay: 1 }}
+                  className="font-source-sans"
+                >
+                  Kepada Yth.
+                </motion.p>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, ease: 'easeOut', delay: 1.5 }}
+                  className="mb-6 text-3xl"
+                >
+                  {guestName}
+                </motion.p>
+                <motion.div
+                  initial={{ y: 20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.5, ease: 'easeOut', delay: 2 }}
+                >
+                  <Button onClick={() => setIsOpen(false)} size="lg">
+                    Buka Undangan
+                  </Button>
+                </motion.div>
               </div>
             </BaseContainer>
           </div>
