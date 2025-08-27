55371a2a10164b4b00881d71b600194f51f3654d
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2025-08-27 20:03:06 +0800

    chore: fix guest name based on url search param

--- src/app/[locale]/(public)/invitation/tata-dan-widya/page.tsx
+++ src/app/[locale]/(public)/invitation/tata-dan-widya/page.tsx
@@ -1,28 +1,33 @@
+import { Toaster } from '@/components/ui/sonner'
 import BaseContainer from '@/shared/components/base-container'
+import CeremonyLocationSection from './_components/ceremony-location-section'
+import CountdownSection from './_components/countdown-section'
 import DateSection from './_components/date-section'
+import GallerySection from './_components/gallery-section'
+import GiftSection from './_components/gift-section'
+import GroomSection from './_components/groom-section'
 import HeroSection from './_components/hero-section'
+import LoveStorySection from './_components/love-story-section'
+import RSVPSection from './_components/rsvp-section'
 import StaticImageBackground from './_components/static-image-background'
 import WelcomeModal from './_components/welcome-modal'
-import GroomSection from './_components/groom-section'
-import CountdownSection from './_components/countdown-section'
-import CeremonyLocationSection from './_components/ceremony-location-section'
-import RSVPSection from './_components/rsvp-section'
-import GallerySection from './_components/gallery-section'
-import LoveStorySection from './_components/love-story-section'
-import GiftSection from './_components/gift-section'
-import { Toaster } from '@/components/ui/sonner'
-import { Copy } from 'lucide-react'
 import WishesSection from './_components/wishes-section'
 
-export default function InvitationPage() {
+interface SearchParams {
+  to?: string
+}
+
+export default async function InvitationPage({ searchParams }: { searchParams: SearchParams }) {
+  const searchQuery = searchParams.to ? decodeURIComponent(searchParams.to) : 'John Doe'
+
   return (
     <>
-      {/* <WelcomeModal guestName="John Doe" /> */}
+      <WelcomeModal guestName={searchQuery} />
       <section className="relative grid grid-cols-1 lg:grid-cols-[1fr_430px] xl:grid-cols-[1fr_500px]">
         {/* Left Content for larger screen */}
         <div className="relative hidden h-full w-full lg:block">
           <StaticImageBackground imageUrl="/static/slide-4.jpg" overlay overlayClass="opacity-25" />
-          <BaseContainer className="relative z-10 flex h-full w-full items-end justify-start text-white">
+          <BaseContainer className="relative z-10 flex h-full w-full max-w-full items-end justify-start text-white">
             <div className="space-y-2 text-white">
               <h2 className="text-xl uppercase">Undangan Pernikahan</h2>
               <h1 className="text-7xl">
