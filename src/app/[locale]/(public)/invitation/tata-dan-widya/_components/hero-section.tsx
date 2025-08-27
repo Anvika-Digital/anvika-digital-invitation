b083c21125ce039c4544780f881930145b82875f
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2025-08-27 21:59:02 +0800

    style: fix font on hero section

--- src/app/[locale]/(public)/invitation/tata-dan-widya/_components/hero-section.tsx
+++ src/app/[locale]/(public)/invitation/tata-dan-widya/_components/hero-section.tsx
@@ -9,11 +9,10 @@ export default function HeroSection() {
       <SliderImageBackground />
       <BaseContainer className="relative z-10 flex h-full justify-between">
         <div className="flex flex-col gap-y-6 text-white">
-          <h1 className="font-noto-serif mt-auto">
+          <h1 className="font-playfair mt-auto">
             <span className="block text-lg">The Wedding Of </span>
             <span className="text-5xl font-medium">
-              Gus Tata
-              <br />& Dayu Widya
+              Gus Tata & <br /> Dayu Widya
             </span>
           </h1>
           <p className="font-medium uppercase">18 September 2025</p>
